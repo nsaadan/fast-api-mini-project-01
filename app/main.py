3bd477ce7f1c639b989f8d1b9dc85953f18f779c
norah <n.alsaadan@thegarage.sa>
2023-06-15 13:06:16 +0300

    feat: connected to postgres db

--- app/main.py
+++ app/main.py
@@ -2,9 +2,18 @@
 
 from fastapi import FastAPI
 from core.config import settings
+from db.session import engine
+from db.base_class import Base
 
-app = FastAPI(title=settings.PROJECT_NAME,version=settings.PROJECT_VERSION)
 
-@app.get("/")
-def hello_api():
-    return {"msg":"Hello API"}
+
+def create_tables():          
+	Base.metadata.create_all(bind=engine)
+        
+def start_application():
+	app = FastAPI(title=settings.PROJECT_NAME,version=settings.PROJECT_VERSION)
+	#app.include_router()
+	create_tables()
+	return app
+
+app = start_application()
